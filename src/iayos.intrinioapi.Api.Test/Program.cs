e0401ecdde1472abacd866649a11a093350f2565
Dale Holborow <dale.holborow@greitco.com>
2016-10-22 16:10:14 +0300

    pub-comp-comp: groundwork - updated ss libs - added xunit for jetbrains - added private appconfig settings

---
+++ src/iayos.intrinioapi.Api.Test/Program.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+
+namespace iayos.intrinioapi.Api.Test
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+
+
+	public class Class1
+	{
+		private IntrinioClient _client;
+
+		public Class1()
+		{
+			var username = "";
+			var password = "";
+
+			_client = new IntrinioClient(username, password);
+		}
+
+
+		[Fact]
+		public void DoSomeStuff()
+		{
+			//_client.
+
+		}
+
+	}
+}
