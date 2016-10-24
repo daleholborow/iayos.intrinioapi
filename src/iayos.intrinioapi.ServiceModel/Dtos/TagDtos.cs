cf11e02a877c3b212d97f1c2466bf4cc4b273ca6
Dale Holborow <dale.holborow@greitco.com>
2016-10-24 12:18:26 +0300

    as-reported: data tags

--- src/iayos.intrinioapi.ServiceModel/Dtos/TagDtos.cs
+++ src/iayos.intrinioapi.ServiceModel/Dtos/TagDtos.cs
@@ -43,4 +43,56 @@ namespace iayos.intrinioapi.ServiceModel.Dtos
 		/// </summary>
 		public string units { get; set; }
 	}
+
+
+	/// <summary>
+	/// http://docs.intrinio.com/#return-values106
+	/// </summary>
+	public class AsReportedTagDto
+	{
+		/// <summary>
+		/// the label of the XBRL tag as reported of the financial statement
+		/// </summary>
+		public string name { get; set; }
+
+		/// <summary>
+		/// the XBRL tag of the associated financial data point - maybe can make this an enum?
+		/// </summary>
+		public string tag { get; set; }
+
+		/// <summary>
+		/// the accounting balance (debit or credit) of the tag
+		/// </summary>
+		public string balance { get; set; }
+
+		/// <summary>
+		/// the unit type of the XBRL tag
+		/// </summary>
+		public string unit { get; set; }
+
+		/// <summary>
+		/// the specified domain XBRL tag, associated with certain data points on the financial statements that have a dimension associated with the data point
+		/// </summary>
+		public object domain_tag { get; set; }
+
+		/// <summary>
+		/// the type of value - true indicates that the value is a header label and has no associated data value, whereas false indicates that the XBRL tag has an associated value.
+		/// </summary>
+		public bool @abstract { get; set; }
+
+		/// <summary>
+		/// an integer 0 or greater indicating the presentation order of the XBRL tag and label in the financial statement
+		/// </summary>
+		public int sequence { get; set; }
+
+		/// <summary>
+		/// an integer 0 or greater indicating the presentation indentation depth of the XBRL tag and label in the financial statement
+		/// </summary>
+		public int depth { get; set; }
+
+		/// <summary>
+		/// + / -
+		/// </summary>
+		public string factor { get; set; }
+	}
 }
--- src/iayos.intrinioapi.ServiceModel/Messages/TagMessages.cs
+++ src/iayos.intrinioapi.ServiceModel/Messages/TagMessages.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using iayos.intrinioapi.ServiceModel.Dtos;
 using iayos.intrinioapi.ServiceModel.Enums;
 using ServiceStack;
@@ -37,4 +38,66 @@ namespace iayos.intrinioapi.ServiceModel.Messages
 	public class GetStandardizedTagsResponse : Response<List<StandardizedTagDto>>
 	{
 	}
+
+
+	[Route("/tags/reported", HttpMethods.Get)]
+	public class GetAsReportedTags : Request, IReturn<GetAsReportedTagsResponse>
+	{
+		/// <summary>
+		///the financial statement requested: income_statement | balance_sheet | cash_flow_statement
+		/// </summary>
+		public FinancialStatement statement { get; set; } = FinancialStatement.IaYoS_Warning_Unset;
+
+		/// <summary>
+		/// (optional, must include a ticker if not specified) - the financial statement tag template: industrial | financial
+		/// </summary>
+		public TagTemplate? template { get; set; }
+
+
+		/// <summary>
+		/// the stock market ticker symbol associated with the companies common stock securities: TICKER SYMBOL
+		/// </summary>
+		public string identifier { get; set; }
+
+		/*
+		public int sequence(optional, returns full list without a sequence number) - an integer 0 or greater for calling a single tag from the first entry, based on order
+		*/
+
+		/// <summary>
+		/// (optional, otherwise, must specify a date) - the fiscal year associated with the fundamental: YYYY
+		/// </summary>
+		public int? fiscal_year { get; set; }
+
+		/// <summary>
+		/// (optional, otherwise, must specify a date) - the fiscal period associated with the fundamental: FY | Q1 | Q2 | Q3
+		/// </summary>
+		public FiscalPeriod? fiscal_period { get; set; }
+
+		/// <summary>
+		/// (optional, otherwise, must specify a fiscal year and fiscal period) - the first fundamental will be the latest as of this specified date: YYYY-MM-DD
+		/// </summary>
+		/// <seealso cref="Date_IaYoS"/>
+		public string date => Date_IaYoS != null ? Date_IaYoS.GetValueOrDefault().ToString("YYYY-MM-DD") : null;
+
+		/// <summary>
+		/// Conveniently set date using actual date object
+		/// </summary>
+		/// <seealso cref="date"/>
+		public DateTime? Date_IaYoS { get; set; }
+
+		/// <summary>
+		/// type (optional, must specify a date or must specify a fiscal year and fiscal period) - the type of periods requested - includes 
+		/// fiscal years for annual data, quarters for quarterly data and trailing twelve months for annual data on a quarterly basis: FY | QTR
+		/// </summary>
+		public FiscalPeriodAsReported type { get; set; }
+
+		/*
+		
+		date (optional, otherwise, must specify a fiscal year and fiscal period) - the first fundamental will be the latest as of this specified date: YYYY-MM-DD
+		*/
+	}
+
+	public class GetAsReportedTagsResponse : Response<List<AsReportedTagDto>>
+{
+}
 }
